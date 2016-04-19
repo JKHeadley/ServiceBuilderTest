9b510f5474b1604582c1d59c985849b514d9df3f
JKHeadley <headley.justin@gmail.com>
2016-04-19 13:36:56 -0700

    Added missing test files.

---
+++ ServiceTests/MyServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceTests.ServiceReference1;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class MyServiceTests
+    {
+        [TestMethod]
+        public void TestSomeServiceCalls()
+        {
+            using (var svc = new ServiceClient())
+            {
+                OrganizationDTO org = new OrganizationDTO { Name = "MyOrg" };
+                var createdOrg = svc.CreateOrganization(org);
+                var test = "cool";
+                createdOrg.Name = "Bobs";
+                var updatedOrg = svc.UpdateOrganization(createdOrg);
+                test = "uhuh";
+                int deleted = svc.DeleteOrganizationById(updatedOrg.Id);
+                test = "yup";
+                var orgs = svc.GetOrganizations();
+                test = "yay!";
+            }
+        }
+    }
+}
